9914f7868a57a655d27b2d6259188cab55dcb416
filatov-apps <92472645+filatov-apps@users.noreply.github.com>
2021-11-04 15:58:00 +0500

    test 3

---
+++ precache-manifest.75e4c925ddea7c7f73c79935a5367eb5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    revision: "79ffe6e44f39c574e334883ef5f7dcee",
+    url: "/luckygames/index.html",
+  },
+  {
+    revision: "62b2836b19f29a03c199",
+    url: "/luckygames/static/css/2.cdee455b.chunk.css",
+  },
+  {
+    revision: "015f252d5ce921a29ef4",
+    url: "/luckygames/static/css/main.29b9a408.chunk.css",
+  },
+  {
+    revision: "62b2836b19f29a03c199",
+    url: "/luckygames/static/js/2.c25b2c96.chunk.js",
+  },
+  {
+    revision: "570d362d673dab785e62d2b8563e1118",
+    url: "/luckygames/static/js/2.c25b2c96.chunk.js.LICENSE.txt",
+  },
+  {
+    revision: "015f252d5ce921a29ef4",
+    url: "/luckygames/static/js/main.e7c3f895.chunk.js",
+  },
+  {
+    revision: "8f3f64d0671efe8159c3",
+    url: "/luckygames/static/js/runtime-main.3807a136.js",
+  },
+]);
--- service-worker.js
+++ service-worker.js
@@ -1,22 +1,9 @@
-/**
- * Welcome to your Workbox-powered service worker!
- *
- * You'll need to register this file in your web app and you should
- * disable HTTP caching for this file too.
- * See https://goo.gl/nhQhGp
- *
- * The rest of the code is auto-generated. Please don't update this file
- * directly; instead, make changes to your Workbox build configuration
- * and re-run your build process.
- * See https://goo.gl/2aRDsh
- */
-
 importScripts(
   "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js"
 );
 
 importScripts(
-  "/luckygames/precache-manifest.3f1cb8707c251391e7224e8c03085f5c.js"
+  "/luckygames/precache-manifest.75e4c925ddea7c7f73c79935a5367eb5.js"
 );
 
 self.addEventListener("message", (event) => {
@@ -27,11 +14,6 @@ self.addEventListener("message", (event) => {
 
 workbox.core.clientsClaim();
 
-/**
- * The workboxSW.precacheAndRoute() method efficiently caches and responds to
- * requests for URLs in the manifest.
- * See https://goo.gl/S9QRab
- */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
