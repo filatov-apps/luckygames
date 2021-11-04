2ab6735bd9c3f6abc00309f26377caf3075ac578
filatov-apps <92472645+filatov-apps@users.noreply.github.com>
2021-11-04 20:30:52 +0500

    LuckyGames
    LuckyGames

---
+++ precache-manifest.da6b7ade4f8c42da09049089fee87c85.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d706c83a64751439ac1836c5ff0ff0b4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "388c6f9c050c6f9786a0",
+    "url": "/static/css/2.cdee455b.chunk.css"
+  },
+  {
+    "revision": "f52b2f8795034b024ec7",
+    "url": "/static/css/main.29b9a408.chunk.css"
+  },
+  {
+    "revision": "388c6f9c050c6f9786a0",
+    "url": "/static/js/2.54fb7936.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.54fb7936.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f52b2f8795034b024ec7",
+    "url": "/static/js/main.a141c9cc.chunk.js"
+  },
+  {
+    "revision": "8f3f64d0671efe8159c3",
+    "url": "/static/js/runtime-main.3807a136.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,16 +11,14 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts(
-  "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js"
-);
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/luckygames/precache-manifest.26a80e83284ee2b273eaa59331a95d10.js"
+  "/precache-manifest.da6b7ade4f8c42da09049089fee87c85.js"
 );
 
-self.addEventListener("message", (event) => {
-  if (event.data && event.data.type === "SKIP_WAITING") {
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
     self.skipWaiting();
   }
 });
@@ -35,9 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(
-  workbox.precaching.getCacheKeyForURL("/luckygames/index.html"),
-  {
-    blacklist: [/^\/_/, /\/[^/?]+\.[^/]+$/],
-  }
-);
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
