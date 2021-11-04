ab418eac0eaddad90040ca25077bdee2c2fa74e7
filatov-apps <92472645+filatov-apps@users.noreply.github.com>
2021-11-04 20:33:21 +0500

    LuckyGames
    LuckyGames

--- precache-manifest.da6b7ade4f8c42da09049089fee87c85.js
+++ precache-manifest.da6b7ade4f8c42da09049089fee87c85.js
@@ -1,30 +1,30 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d706c83a64751439ac1836c5ff0ff0b4",
-    "url": "/index.html"
+    revision: "d706c83a64751439ac1836c5ff0ff0b4",
+    url: "/luckygames/index.html",
   },
   {
-    "revision": "388c6f9c050c6f9786a0",
-    "url": "/static/css/2.cdee455b.chunk.css"
+    revision: "388c6f9c050c6f9786a0",
+    url: "/luckygames/static/css/2.cdee455b.chunk.css",
   },
   {
-    "revision": "f52b2f8795034b024ec7",
-    "url": "/static/css/main.29b9a408.chunk.css"
+    revision: "f52b2f8795034b024ec7",
+    url: "/luckygames/static/css/main.29b9a408.chunk.css",
   },
   {
-    "revision": "388c6f9c050c6f9786a0",
-    "url": "/static/js/2.54fb7936.chunk.js"
+    revision: "388c6f9c050c6f9786a0",
+    url: "/luckygames/static/js/2.54fb7936.chunk.js",
   },
   {
-    "revision": "570d362d673dab785e62d2b8563e1118",
-    "url": "/static/js/2.54fb7936.chunk.js.LICENSE.txt"
+    revision: "570d362d673dab785e62d2b8563e1118",
+    url: "/luckygames/static/js/2.54fb7936.chunk.js.LICENSE.txt",
   },
   {
-    "revision": "f52b2f8795034b024ec7",
-    "url": "/static/js/main.a141c9cc.chunk.js"
+    revision: "f52b2f8795034b024ec7",
+    url: "/luckygames/static/js/main.a141c9cc.chunk.js",
   },
   {
-    "revision": "8f3f64d0671efe8159c3",
-    "url": "/static/js/runtime-main.3807a136.js"
-  }
+    revision: "8f3f64d0671efe8159c3",
+    url: "/luckygames/static/js/runtime-main.3807a136.js",
+  },
 ]);
--- service-worker.js
+++ service-worker.js
@@ -11,14 +11,16 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
-
 importScripts(
-  "/precache-manifest.da6b7ade4f8c42da09049089fee87c85.js"
+  "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js"
 );
 
-self.addEventListener('message', (event) => {
-  if (event.data && event.data.type === 'SKIP_WAITING') {
+importScripts(
+  "/luckygames/precache-manifest.da6b7ade4f8c42da09049089fee87c85.js"
+);
+
+self.addEventListener("message", (event) => {
+  if (event.data && event.data.type === "SKIP_WAITING") {
     self.skipWaiting();
   }
 });
@@ -33,7 +35,9 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
-  
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
-});
+workbox.routing.registerNavigationRoute(
+  workbox.precaching.getCacheKeyForURL("/luckygames/index.html"),
+  {
+    blacklist: [/^\/_/, /\/[^/?]+\.[^/]+$/],
+  }
+);
