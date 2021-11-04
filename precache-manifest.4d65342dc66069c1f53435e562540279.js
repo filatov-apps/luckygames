2fcee798be870f083e88e7b8888156b2255e7e0f
filatov-apps <92472645+filatov-apps@users.noreply.github.com>
2021-11-04 16:17:40 +0500

    beta test

---
+++ precache-manifest.4d65342dc66069c1f53435e562540279.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    revision: "fe2f4dfe2e4f7597a0d128076e99e6fc",
+    url: "/luckygames/index.html",
+  },
+  {
+    revision: "3e2ba235a3bc8aa49063",
+    url: "/luckygames/static/css/2.cdee455b.chunk.css",
+  },
+  {
+    revision: "102d79b1dd52c1f5eadf",
+    url: "/luckygames/static/css/main.29b9a408.chunk.css",
+  },
+  {
+    revision: "3e2ba235a3bc8aa49063",
+    url: "/luckygames/static/js/2.63d04018.chunk.js",
+  },
+  {
+    revision: "570d362d673dab785e62d2b8563e1118",
+    url: "/luckygames/static/js/2.63d04018.chunk.js.LICENSE.txt",
+  },
+  {
+    revision: "102d79b1dd52c1f5eadf",
+    url: "/luckygames/static/js/main.ef1b948d.chunk.js",
+  },
+  {
+    revision: "8f3f64d0671efe8159c3",
+    url: "/luckygames/static/js/runtime-main.3807a136.js",
+  },
+]);
